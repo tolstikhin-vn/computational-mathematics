37239c4f5016defa46c04941ec2c570a6a6edca7
Vladimir Tolstikhin <vr.tn1602@mail.ru>
2022-11-19 00:03:23 +0300

    Реализованы методы скорейшего спуска и минимальной невязки для СЛАУ

---
+++ HomeWork-4/MinimalResidualsAndSpeedyDescent/src/MRAndSDMethods.java
@@ -0,0 +1,123 @@
+/*Для заданных систем линейных алгебраических уравнений реализуйте методы поиска численных решений методами
+минимальных невязок и скорейшего спуска.*/
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.DoubleSummaryStatistics;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class MRAndSDMethods {
+    private static final int NORMS_NUMBER = 3;
+    private static final String[] LIST_OF_NORMS = new String[]{"||X||∞", "||X||₁", "||X||₂ₗ"};
+
+    private static final int L_VALUE = 2;  // Параметр для нормы ||X||₂ₗ
+
+    public static void main(String[] args) {
+
+        int[] degrees = new int[]{2, 3, 4, 5, 10, 12, 15}; // Степени десятки для точности ε
+        LinkedList<double[][]> listOfMatrices1 = new LinkedList<>(); // Коллекция из массивов систем
+        LinkedList<double[]> listOfMatrices2 = new LinkedList<>(); // Коллекция из столбцов свободных членов
+
+        fileReading(listOfMatrices1, listOfMatrices2);
+
+        SpeedyDescent speedyDescent = new SpeedyDescent();
+        MinimalResiduals minimalResiduals = new MinimalResiduals();
+        for (int i = 0; i < listOfMatrices1.size(); ++i) {
+            System.out.println("Система № " + (i + 1));
+            for (int degree : degrees) {
+                for (int j = 0; j < NORMS_NUMBER; ++j) {
+                    System.out.println("Норма " + LIST_OF_NORMS[j]);
+                    // Вывод результатов
+                    System.out.println("Метод минимальных невязок");
+                    minimalResiduals.findSolution(listOfMatrices1.get(i), listOfMatrices2.get(i), degree, (j + 1));
+                    System.out.println();
+                    System.out.println("Метод наискорейшего спуска");
+                    speedyDescent.findSolution(listOfMatrices1.get(i), listOfMatrices2.get(i), degree, (j + 1));
+                    System.out.println();
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    // Чтение файла, содержащего системы уравнений
+    public static void fileReading(LinkedList<double[][]> listOfMatrices1, LinkedList<double[]> listOfMatrices2) {
+        File file = new File("input.txt");
+        if (file.length() == 0) {
+            System.out.println("Файл пуст");
+        } else {
+            ArrayList<String[]> coefficients = new ArrayList<>();
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                String str;
+                while ((str = reader.readLine()) != null) {
+                    if (!str.equals("")) {
+                        coefficients.add(str.split(" "));
+                    } else {
+                        fillingListOfMatrices(coefficients, listOfMatrices1, listOfMatrices2);
+                        coefficients.clear();
+                    }
+                }
+                // Заполнение последним массивов
+                fillingListOfMatrices(coefficients, listOfMatrices1, listOfMatrices2);
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
+    // Заполнение коллекции с массивами систем
+    public static void fillingListOfMatrices(ArrayList<String[]> coefficients, LinkedList<double[][]> listOfMatrices1, LinkedList<double[]> listOfMatrices2) {
+        int columns = coefficients.get(0).length;
+        double[][] system1 = new double[coefficients.size()][columns - 1];
+        double[] system2 = new double[coefficients.size()];
+        Iterator<String[]> iter = coefficients.iterator();
+        for (int i = 0; i < system1.length; ++i) {
+            String[] s = iter.next();
+            for (int j = 0; j < columns; ++j) {
+                if (j == columns - 1) {
+                    system2[i] = Double.parseDouble(s[j]);
+                    continue;
+                }
+                system1[i][j] = Double.parseDouble(s[j]);
+            }
+        }
+        listOfMatrices1.add(system1);
+        listOfMatrices2.add(system2);
+    }
+
+    // Поиск нормы исходя из текущей рассчитываемой нормы
+    public static double findNorm(int numOfNorm, int size, double[] currentVariableValues, double[] previousVariableValues) {
+        double norm = 0;
+        switch (numOfNorm) {
+            // Для нормы ||X||∞
+            case (1) -> {
+                double[] values = new double[size];
+                for (int i = 0; i < size; ++i) {
+                    values[i] = Math.abs(currentVariableValues[i] - previousVariableValues[i]);
+                }
+                DoubleSummaryStatistics stat = Arrays.stream(values).summaryStatistics();
+                return stat.getMax();
+            }
+            // Для нормы ||X||₁
+            case (2) -> {
+                for (int i = 0; i < size; ++i) {
+                    norm += Math.abs(currentVariableValues[i] - previousVariableValues[i]);
+                }
+                return norm;
+            }
+            // Для нормы ||X||₂ₗ
+            case (3) -> {
+                for (int i = 0; i < size; ++i) {
+                    norm += Math.pow(currentVariableValues[i] - previousVariableValues[i], 2 * L_VALUE);
+                }
+                return Math.pow(norm, 1. / (2 * L_VALUE));
+            }
+        }
+        return 0;
+    }
+}
---
+++ HomeWork-4/MinimalResidualsAndSpeedyDescent/src/SpeedyDescent.java
@@ -0,0 +1,99 @@
+import java.util.Arrays;
+public class SpeedyDescent {
+
+    public void findSolution(double[][] initialMatrix, double[] freeMembers, int n, int numOfNorm) {
+
+        String epsilon = "10^(-" + n + ")";
+        double[] currentXValues = new double[initialMatrix.length];  // Текущие приближения
+        double[] prevXValues; // Предыдущие приближения
+        double[] rValues; // Вектор невязки
+        double paramT; // Длина шага вдоль направления градиента
+        int iterationsNumber = 0; // Количество итераций
+        double norm = 0; // Норма
+        int size = initialMatrix.length; // Размерность исходной матрицы
+        boolean convergenceConditionIsSatisfied = true;
+
+        do {
+            // Находим вектор невязок на текущем шаге
+            rValues = subtracting(multiplyMatrices(initialMatrix, currentXValues), freeMembers);
+
+            // Определяем длину шага вдоль направления градиента
+            paramT = scalarProduct(rValues, rValues) / scalarProduct(multiplyMatrices(initialMatrix, rValues), rValues);
+
+            if (paramT == 0 || norm > 100) {
+                convergenceConditionIsSatisfied = false;
+                break;
+            }
+
+            // Инициализируем прерыдущие приближения текущими (до изменений)
+            prevXValues = currentXValues;
+
+            // Находим новое приближение на текущем шаге
+            currentXValues = subtracting(currentXValues, findResidualVector(paramT, rValues));
+
+            // Считаем норму
+            norm = MRAndSDMethods.findNorm(numOfNorm, size, currentXValues, prevXValues);
+
+            ++iterationsNumber;
+
+        } while (norm > Math.pow(10, -n));
+
+        // Вывод результатов вычислений
+        if (convergenceConditionIsSatisfied) {
+            System.out.println(printResult(currentXValues, epsilon, iterationsNumber));
+        } else {
+            System.out.println("Метод не сходится");
+        }
+    }
+
+    // Вычисление разности двух матриц
+    private double[] subtracting(double[] first, double[] second) {
+        double[] result = new double[first.length];
+        for (int i = 0; i < first.length; ++i) {
+            result[i] = first[i] - second[i];
+        }
+        return result;
+    }
+
+    // Нахождение скалярного произведения
+    private double scalarProduct(double[] first, double[] second) {
+        double result = 0;
+        for (int i = 0; i < first.length; ++i) {
+            result += first[i] * second[i];
+        }
+        return result;
+    }
+
+    // Вычисление вектора невязки путем умножения матрицы на вектор
+    private double[] findResidualVector(double param, double[] actual) {
+        double[] residualVector = new double[actual.length];
+        for (int i = 0; i < actual.length; ++i) {
+            residualVector[i] = param * actual[i];
+        }
+        return residualVector;
+    }
+
+    // Нахождение результата перемножения двух матрицы
+    private double[] multiplyMatrices(double[][] firstMatrix, double[] secondMatrix) {
+        double[] result = new double[firstMatrix.length];
+
+        for (int row = 0; row < result.length; row++) {
+            result[row] = multiplyMatricesCell(firstMatrix, secondMatrix, row);
+        }
+
+        return result;
+    }
+
+    double multiplyMatricesCell(double[][] firstMatrix, double[] secondMatrix, int row) {
+        double cell = 0;
+        for (int i = 0; i < secondMatrix.length; i++) {
+            cell += firstMatrix[row][i] * secondMatrix[i];
+        }
+        return cell;
+    }
+
+    // Вывод полученных результатов
+    public String printResult(double[] previousVariableValues, String epsilon, int iterationsNumber) {
+        return Arrays.toString(previousVariableValues) + " ε = " + epsilon + " итераций: " + iterationsNumber;
+    }
+}
